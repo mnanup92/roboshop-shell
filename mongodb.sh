345cde990c65f0838efdf5758bdc1b8aea728066
anupn <anup.natta@gmail.com>
2025-09-29 13:35:13 +0530

    editing mogodb ip

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,8 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
+
+sed -i -e '/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
